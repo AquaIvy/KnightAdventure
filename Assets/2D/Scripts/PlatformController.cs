916247e3075f03c7fa31db9822f426a214e4e946
Lv Home <lvjx1@lenovo.com>
2020-05-13 23:28:22 +0800

    .

---
+++ Assets/2D/Scripts/PlatformController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KnightAdventure
+{
+    public class PlatformController : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] androidPlatformInputGameObjects;
+
+        private void Awake()
+        {
+            bool androidPlatform = Application.platform == RuntimePlatform.Android;
+
+            for (int i = 0; i < androidPlatformInputGameObjects.Length; i++)
+            {
+                androidPlatformInputGameObjects[i].SetActive(androidPlatform);
+            }
+        }
+    }
+}
